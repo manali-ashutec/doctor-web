d3bea00baa8e6cdca54c82791b4c2e52175aa222
manali-ashute <manali@ashutec.com>
2021-10-05 12:25:52 +0530

    last commite

--- js/script.js
+++ js/script.js
@@ -9,31 +9,27 @@ iconEl.addEventListener("click",function(){
 
 //////up aero////////
 const upSecEl = document.querySelector(".open-up");
-const headerEl = document.getElementsByTagName("header");
 const upToEl = document.querySelector(".btn-aero");
 
 const  obs = new IntersectionObserver(
     function(entries){
         const ent = entries[0];
-        console.log(ent);
-        
-    if (ent.isIntersecting == false) {
-        upToEl.classList.add("move");
-        headerEl[0].classList.add("sticky");
-    }
-   
-    if (ent.isIntersecting == true) {
-       upToEl.classList.remove("move");
-       headerEl[0].classList.remove("sticky");
-    }
-},
-{
+
+        if (ent.isIntersecting == false) {
+            upToEl.classList.add("move");
+        }
+    
+        if (ent.isIntersecting == true) {
+            upToEl.classList.remove("move");
+        }
+    },
+    {
     //In the vie port
     root:null, ///where did element should be appiaring or not
     threshold: 0  ,  /// out side the hero section, 1 means conplete hero section.
     rootMargin: "-80px"
-})
-obs.observe(upSecEl);
+    })
+    obs.observe(upSecEl);
 
 upToEl.addEventListener("click",function(e){
     e.preventDefault();
